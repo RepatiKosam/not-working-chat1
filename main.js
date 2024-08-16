fbbc59cf09f8225e744ff693ffcad14e0ceb9122
RepatiKosam <mallidishivareddy718@gmail.com>
2024-08-16 12:40:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+let username = localStorage.getItem("username");
+if(!username){
+    username = window.prompt("What's your name?");
+    localStorage.setItem("username",username);
+}
+const userMessage = document.getElementById("user-message");
+const sendButton = document.getElementById("send");
+const container = document.getElementById("container");
+sendButton.addEventListener("click",function(){
+    if(userMessage.value !== ""){
+        createMessageBody();
+        userMessage.value = "";
+        userMessage.focus();
+    }
+});
+document.addEventListener("keydown",function(event){
+    if(event.key == "Enter"){
+        if(userMessage.value !== ""){
+            createMessageBody();
+            userMessage.value = "";
+            userMessage.focus();
+        }
+    }
+    
+});
+function createMessageBody(){
+    let box = document.createElement("div");
+    let name = document.createElement("h4");
+    let msg = document.createElement("p");
+    let breakElement = document.createElement("br");
+    let time = document.createElement("h6");
+    box.className = "message";
+    name.textContent = username;
+    msg.textContent = userMessage.value;
+    let now = new Date();
+    time.textContent = `${now.getHours().toString().padStart(2,0)}:${now.getMinutes().toString().padStart(2,0)}`;
+    box.append(name);
+    box.append(breakElement);
+    box.append(msg);
+    box.append(time);
+    document.getElementById("container").append(box);
+}
+
+
+
